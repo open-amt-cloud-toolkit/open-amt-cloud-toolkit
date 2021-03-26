5e6877f5e82cc474cd3ed5d41e43546dc3e45d41
madhavi-losetty <madhavi.losetty@intel.com>
2021-03-26 10:47:26 -0700

    fix(sqlscript): added citext to db script to accept case insensitive data

--- data/init.sql
+++ data/init.sql
@@ -2,9 +2,9 @@
 * Copyright (c) Intel Corporation 2020
 * SPDX-License-Identifier: Apache-2.0
 **********************************************************************/
-
+CREATE EXTENSION IF NOT EXISTS citext;
 CREATE TABLE IF NOT EXISTS ciraconfigs(
-      cira_config_name varchar(40) NOT NULL,
+      cira_config_name citext NOT NULL,
       mps_server_address varchar(256),
       mps_port integer,
       user_name varchar(40),
@@ -17,24 +17,24 @@ CREATE TABLE IF NOT EXISTS ciraconfigs(
       CONSTRAINT configname UNIQUE(cira_config_name)
     );
 CREATE TABLE IF NOT EXISTS networkconfigs(
-      network_profile_name varchar(40) NOT NULL,
+      network_profile_name citext NOT NULL,
       dhcp_enabled BOOLEAN NOT NULL,
       static_ip_shared BOOLEAN NOT NULL,
       ip_sync_enabled BOOLEAN NOT NULL,
       CONSTRAINT networkprofilename UNIQUE(network_profile_name)
     );
 CREATE TABLE IF NOT EXISTS profiles(
-      profile_name varchar(40) NOT NULL,
+      profile_name citext NOT NULL,
       activation varchar(20) NOT NULL,
       amt_password varchar(40),
       configuration_script text,
-      cira_config_name varchar(40) REFERENCES ciraconfigs(cira_config_name),
+      cira_config_name citext REFERENCES ciraconfigs(cira_config_name),
       generate_random_password BOOLEAN NOT NULL,
       random_password_characters varchar(100),
       random_password_length integer,
       creation_date timestamp,
       created_by varchar(40),
-      network_profile_name varchar(40) REFERENCES networkconfigs(network_profile_name),
+      network_profile_name citext REFERENCES networkconfigs(network_profile_name),
       mebx_password varchar(40),
       generate_random_mebx_password BOOLEAN NOT NULL,
       random_mebx_password_length integer,
@@ -42,8 +42,8 @@ CREATE TABLE IF NOT EXISTS profiles(
       CONSTRAINT name UNIQUE(profile_name)
     );
 CREATE TABLE IF NOT EXISTS domains(
-      name varchar(40) NOT NULL,
-      domain_suffix varchar(40) NOT NULL,
+      name citext NOT NULL,
+      domain_suffix citext NOT NULL,
       provisioning_cert text,
       provisioning_cert_storage_format varchar(40),
       provisioning_cert_key text,
@@ -53,9 +53,15 @@ CREATE TABLE IF NOT EXISTS domains(
       CONSTRAINT domainsuffix UNIQUE(domain_suffix)
     );
 
+CREATE UNIQUE INDEX lower_cira_config_name_idx ON ciraconfigs ((lower(cira_config_name)));
+CREATE UNIQUE INDEX lower_network_profile_name_idx ON networkconfigs ((lower(network_profile_name)));
+CREATE UNIQUE INDEX lower_profile_name_idx ON profiles ((lower(profile_name)));
+CREATE UNIQUE INDEX lower_name_suffix_idx ON domains ((lower(name)), (lower(domain_suffix)));
+
 INSERT INTO public.networkconfigs(
   network_profile_name, dhcp_enabled, static_ip_shared, ip_sync_enabled) 
   values('dhcp_disabled', false, true, true);
 INSERT INTO public.networkconfigs(
   network_profile_name, dhcp_enabled, static_ip_shared, ip_sync_enabled)  
   values('dhcp_enabled', true, false, true);
+
